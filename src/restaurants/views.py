368419ead00c2ee18d96013741146c438bd833c8
aminhp93 <minhpn.org.ec@gmail.com>
2017-07-10 12:06:16 +0700

    5 - Render Django Template

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -1,12 +1,15 @@
+import random
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
 def home(request):
-	template = ""
+	num = random.randint(0, 111111)
+	template = "base.html"
 
-	context = {}
-	return HttpResponse("Hello workd")
+	context = {"context_var": "True", "num": num}
+	
 
-	# return render(request, template, context)
+	return render(request, template, context)

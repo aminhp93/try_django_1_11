0ed2d0f063a9f9cfce211b8ae0a879561149e29e
aminhp93 <minhpn.org.ec@gmail.com>
2017-07-10 13:11:49 +0700

    6 - Context in Django Template

--- src/restaurants/views.py
+++ src/restaurants/views.py
@@ -7,9 +7,14 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
 	num = random.randint(0, 111111)
+	lists = [2, 5, 6]
 	template = "base.html"
 
-	context = {"context_var": "True", "num": num}
-	
+	context = {
+			"context_var": "True",
+			"num": num, 
+			"bool_item": True,
+			"lists": lists
+	}
 
 	return render(request, template, context)

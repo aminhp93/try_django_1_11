c66ef785b7f1535df3600cbfec13078e865bf35e
aminhp93 <minhpn.org.ec@gmail.com>
2017-07-10 21:50:25 +0700

    20 - SlugField & Unique Slug Generator

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -7,7 +7,13 @@ class RestaurantLocation(models.Model):
 	category 	= models.CharField(max_length=120, null=True, blank=True)
 	timestamp 	= models.DateTimeField(auto_now_add=True)
 	updated 	= models.DateTimeField(auto_now=True)
+	slug		= models.SlugField(null=True, blank=True)
 	
 	def __str__(self):
 		return self.name
+
+	@property
+	def title(self):
+		return self.name
+	
 

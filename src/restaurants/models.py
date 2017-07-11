6d8c9cd1b0f4501326b2809ba3208cc108203c31
aminhp93 <minhpn.org.ec@gmail.com>
2017-07-11 09:02:27 +0700

    21 - Signal for Unique Slug

--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import pre_save, post_save
+
+from .utils import unique_slug_generator
 
 # Create your models here.
 class RestaurantLocation(models.Model):
@@ -16,4 +19,19 @@ class RestaurantLocation(models.Model):
 	def title(self):
 		return self.name
 	
+def rl_pre_save_receiver(sender, instance, *args, **kwargs):
+	print("saving")
+	print(instance.timestamp)
+	if not instance.slug:
+		instance.slug = unique_slug_generator(instance)
+	
+# def rl_post_save_receiver(sender, instance, created, *args, **kwargs):
+# 	print("saved")
+# 	print(instance.timestamp)
+# 	if not instance.slug:
+# 		instance.slug = unique_slug_generator(instance)
+# 		instance.save()
 
+pre_save.connect(rl_pre_save_receiver, sender=RestaurantLocation)
+
+# post_save.connect(rl_post_save_receiver, sender=RestaurantLocation)

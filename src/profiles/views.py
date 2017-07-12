66e386a8a06e472d87b55eed9fd0b5e7eafff502
aminhp93 <minhpn.org.ec@gmail.com>
2017-07-12 09:29:38 +0700

    40 - Adding a Robust Search

--- src/profiles/views.py
+++ src/profiles/views.py
@@ -4,6 +4,9 @@ from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
 # Create your views here.
 
+from menus.models import Item
+from restaurants.models import RestaurantLocation
+
 User = get_user_model()
 
 class ProfileDetailView(DetailView):
@@ -14,3 +17,18 @@ class ProfileDetailView(DetailView):
 		if username is None:
 			raise Http404
 		return get_object_or_404(User, username__iexact=username, is_active=True)
+
+	def get_context_data(self, *args, **kwargs):
+		context = super().get_context_data(*args, **kwargs)
+		print(context)
+		# user = self.get_object()
+		user = context['user']
+		query = self.request.GET.get('q')
+		print(user)
+		item_exists = Item.objects.filter(user=user).exists()
+		qs = RestaurantLocation.objects.filter(owner=user)
+		if query:
+			qs = qs.search(query)
+		if item_exists and qs.exists():
+			context['locations'] = qs
+		return context
--- src/restaurants/models.py
+++ src/restaurants/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.core.urlresolvers import reverse
 from django.db import models
+from django.db.models import Q
 from django.db.models.signals import pre_save, post_save
 
 from .utils import unique_slug_generator
@@ -8,6 +9,25 @@ from .utils import unique_slug_generator
 User = settings.AUTH_USER_MODEL
 
 # Create your models here.
+
+class RestaurantLocationQuerySet(models.query.QuerySet):
+	def search(self, query):
+		return self.filter(
+			Q(name__icontains=query)|
+			Q(location__icontains=query)|
+			Q(category__icontains=query)|
+			Q(item__name__icontains=query)|
+			Q(item__content__icontains=query)|
+			Q(item__exclude__icontains=query)
+		)
+
+class RestaurantLocationManager(models.Manager):
+	def get_queryset(self):
+		return RestaurantLocationQuerySet(self.model, using=self._db)
+
+	def search(self, query):
+		return self.get_queryset().search(query)
+
 class RestaurantLocation(models.Model):
 	owner 		= models.ForeignKey(User)
 	name 		= models.CharField(max_length=120)
@@ -16,6 +36,8 @@ class RestaurantLocation(models.Model):
 	timestamp 	= models.DateTimeField(auto_now_add=True)
 	updated 	= models.DateTimeField(auto_now=True)
 	slug		= models.SlugField(null=True, blank=True)
+
+	objects = RestaurantLocationManager()
 	
 	def __str__(self):
 		return self.name
